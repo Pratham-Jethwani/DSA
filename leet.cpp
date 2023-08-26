f4891ffa3bcbbaec56fd1721a8b0ff83ff7bcdfb
Pratham-Jethwani <prathamjethwani52@gmail.com>
2023-08-26 11:18:54 +0530

    ques

--- leet.cpp
+++ leet.cpp
@@ -28,7 +28,7 @@ int main()
 /*
 two's complement
 */
-
+/*
 #include<iostream>
 #include<math.h>
 using namespace std;
@@ -62,3 +62,83 @@ int main()
     }
     cout<<comp;
 }
+*/
+
+/*
+two check if the given integer can be written in power of 2.
+*/
+/*
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n,flag=0;
+    cout<<"enter the number: ";
+    cin>>n;
+    for(int i=0;i<=30;i++)              //int ranges from (2^-31,2^31-1)
+    {
+        if(pow(2,i)==n)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        cout<<"true";
+    }
+    else
+    {
+        cout<<"false";
+    }
+}
+*/
+
+/*
+number of notes of 100,50,20,1 required for amount using switch case
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the amount ";
+    cin>>n;
+    int notes[4]={100,50,20,1};
+    
+    for(int i=0;i<sizeof(notes)-1;i++)
+    {
+        switch(notes[i])
+        {
+            case 100:
+            cout<<"Number of 100Rs note: "<<n/100<<endl;
+            n=n%100;
+            break;
+
+
+            case 50:
+            cout<<"Number of 50Rs note: "<<n/50<<endl;
+            n=n%50;
+            break;
+            
+
+            case 20:
+            cout<<"Number of 20Rs note: "<<n/20<<endl;
+            n=n%20;
+            break;
+            
+
+            case 1:
+            cout<<"Number of 1Rs note: "<<n/1<<endl;
+            n=n%1;
+            break;
+        }
+        if(n==0)
+        {
+            break;
+        }
+    }
+    
+}
+
